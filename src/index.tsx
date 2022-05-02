ea7d3ef14084557e9ac9bbbc318ed60aa0136f61
dingsheng <dingduoduo0916@gmail.com>
2022-05-02 11:10:04 +0800

    feat； 引入prettier 在git commit阶段自动格式化
    feat: 引入prettier
    
    feat: 引入prettier
    
    feat: 引入prettier
    
    feat: 引入prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+console.log(2);
 root.render(
   <React.StrictMode>
     <App />
